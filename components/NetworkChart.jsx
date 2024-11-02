24f1dfc735a71a96db0b055e8e5b0411ce8d013d
madhat5 <davidmalary@gmail.com>
2024-11-02 12:37:39 -0400

    feat: section placeholders

---
+++ components/NetworkChart.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+
+const specs = {
+  margin: {
+    top: 30,
+    right: 25,
+    bottom: 70,
+    left: 50
+  }
+};
+
+// const NetworkChart = ({ width, height, data }) => {
+const NetworkChart = () => {
+
+  return (
+    <div className="container">
+      network chart placeholder
+    </div>
+  );
+};
+
+export default NetworkChart;
