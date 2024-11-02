accc97eca17a975d8692733d9d6bb10eab3dbc72
madhat5 <davidmalary@gmail.com>
2024-11-01 22:01:41 -0400

    feat: init commit

---
+++ components/network_chart.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+
+const specs = {
+  margin: {
+    top: 30,
+    right: 25,
+    bottom: 70,
+    left: 50
+  }
+};
+
+const NetworkChart = ({ width, height, data }) => {
+
+  return ();
+};
+
+export default NetworkChart;
